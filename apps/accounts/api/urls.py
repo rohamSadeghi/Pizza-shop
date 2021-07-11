7a4f59360066e18e5b844496e715628e1b5e25af
roham <roham.sa88@gmail.com>
2021-07-11 11:24:28 +0430

    Remove extra router urls

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -1,14 +1,10 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from apps.accounts.api.views import TokenObtainPairView, TokenRefreshView
 
-
-router = routers.SimpleRouter()
 
 urlpatterns = [
     path('token/obtain/', TokenObtainPairView.as_view(), name='token-obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 
-    path('', include(router.urls)),
 ]
